db122ce65ada642733cb47d1a24562f855eb8fa1
Midchow <midchow@gmail.com>
2016-12-03 22:39:18 +0900

    [iOS] UnityAds SDK추가(버전 2.0.6)

--- Timefish/Classes/ios/UnityAdsX.mm
+++ Timefish/Classes/ios/UnityAdsX.mm
@@ -8,6 +8,15 @@
 
 #include "UnityAdsX.h"
 
+#import "UnityAds/UnityAds.h"
+
+#define UnityAdsGameID  @"1220569"
+#define integrationID   @"rewardedVideo"
+
+@interface UnityAdsDelegateBridge : NSObject<UnityAdsDelegate>
+@end
+
+static UnityAdsDelegateBridge* s_pUADelegateBridge = nil;
 static UnityAdsX* s_pUnityAdsX = NULL;
 
 UnityAdsX* UnityAdsX::getInstance()
@@ -15,6 +24,10 @@ UnityAdsX* UnityAdsX::getInstance()
     if (s_pUnityAdsX == NULL)
     {
         s_pUnityAdsX = new UnityAdsX();
+        s_pUADelegateBridge = [[UnityAdsDelegateBridge alloc] init];
+
+        //
+        [UnityAds initialize:UnityAdsGameID delegate:s_pUADelegateBridge];
     }
     return s_pUnityAdsX;
 }
@@ -26,11 +39,46 @@ void UnityAdsX::startSession()
 
 void UnityAdsX::showVideo()
 {
-    // do nothing here
+    UIViewController* rootViewController;
+    rootViewController = [UIApplication sharedApplication].keyWindow.rootViewController;
+    [UnityAds show:rootViewController placementId:integrationID];
 }
 
 bool UnityAdsX::isVideoAvailable()
 {
-    // not available
-    return false;
+    bool isAvailable = [UnityAds isReady:integrationID];
+    CCLOG("isVideoAvailable: %d", isAvailable);
+    return isAvailable;
 }
+
+#pragma mark -
+#pragma mark UnityAdsDelegateBridge
+@implementation UnityAdsDelegateBridge
+- (void)unityAdsReady:(NSString *)placementId{
+    CCLOG("unityAdsReady: %s", [placementId UTF8String]);
+}
+
+- (void)unityAdsDidError:(UnityAdsError)error withMessage:(NSString *)message
+{
+}
+
+- (void)unityAdsDidStart:(NSString *)placementId
+{
+    if (UnityAdsXDelegate* delegate = UnityAdsX::getInstance()->getDelegate()) {
+        delegate->onUnityAdsStart();
+    }
+}
+
+- (void)unityAdsDidFinish:(NSString *)placementId withFinishState:(UnityAdsFinishState)state
+{
+    bool shown = false;
+    if (state == kUnityAdsFinishStateCompleted) {
+        shown = true;
+    }
+    
+    //
+    if (UnityAdsXDelegate* delegate = UnityAdsX::getInstance()->getDelegate()) {
+        delegate->onUnityAdsFinish(shown);
+    }
+}
+@end
